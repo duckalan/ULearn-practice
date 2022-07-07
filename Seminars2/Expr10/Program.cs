985e0c9d9ebc64e47d9ec813b6e6896b59e13642
duckalan <67800818+duckalan@users.noreply.github.com>
2022-07-07 11:51:32 +0300

    My code

---
+++ Seminars2/Expr10/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Expr10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = SumOfNumbersMultipleThreeOrFive();
+            Console.WriteLine(result);
+        }
+
+        /// <summary>
+        /// Вычисляет сумму всех чисел от 1 до 1000, кратных 3 или 5.
+        /// </summary>
+        static int SumOfNumbersMultipleThreeOrFive()
+        {
+            int result = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i % 3) == 0 || (i % 5) == 0)
+                {
+                    result += i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
